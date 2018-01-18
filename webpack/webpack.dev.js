ddb8f9f19f4f6506d7534733bbd220a7bdd7f2eb
liyangwood <liyangwood@gmail.com>
2018-01-18 17:16:10 +0800

    update for running in windows, notice package.json

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -25,6 +25,12 @@ const webpackConfig = {
 	},
 	// A SourceMap without column-mappings ignoring loaded Source Maps.
 	devtool: 'cheap-module-source-map',
+    devServer: {
+        contentBase: resolve('./public'),
+        hot: true,
+        host: '0.0.0.0',
+        port: 3000,
+    },
 	module: {
 		rules: [
 			{
